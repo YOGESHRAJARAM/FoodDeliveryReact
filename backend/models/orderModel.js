bea6e5c064b8bf07ad5574a2496d35956b2e70c8
YOGESH RAJARAM <yogiyogeshrajaram@gmail.com>
2024-07-02 18:09:59 +0530

    sample

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
     userId:{type:String,require:true},
-    items:{type:Arry,required:true},
+    items:{type:Array,required:true},
     amount:{type:Number,required:true},
     address:{type:Object,required:true},
-    status:{type:string,default:"Food Processing"},
+    status:{type:String,default:"Food Processing"},
     date:{type:Date,default:Date.now()},
     payment:{type:Boolean,default:false}
 })
