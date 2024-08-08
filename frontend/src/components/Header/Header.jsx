6137f647c88f6f96e0f7b20c4c93efd4c62572ff
YOGESH RAJARAM <yogiyogeshrajaram@gmail.com>
2024-08-08 11:10:55 +0530

    headerwallpaper corrected

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <div className="header">
         <div className="header-contents">
-            <h2>Order Your favourit food here</h2>
+            <h2>Order Your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable arry of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience , one delicious meal at a time.</p>
             {/* <button>View Menu</button> */}
   
